b9d2a7aa4b7f62123253127183f646712c5b8a71
leonard-captivatechat <leonard@captivatechat.com>
2023-11-16 22:04:07 +0800

    ✨ Init auth pages

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+export default function AccountAuthLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <section
+      id="account-layout"
+      className="h-screen bg-[url('/images/bg-account.png')] bg-contain bg-no-repeat"
+    >
+      <div className="flex h-full justify-end">
+        <div className="mr-28 flex w-[450px] items-center">{children}</div>
+      </div>
+    </section>
+  )
+}
