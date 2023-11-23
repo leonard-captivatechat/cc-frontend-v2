23de36d3482cb27e7e53eefaf8d63c63f15da51b
leonard-captivatechat <leonard@captivatechat.com>
2023-11-23 20:14:15 +0800

    💄 Secondary dark style

--- tailwind.config.js
+++ tailwind.config.js
@@ -50,6 +50,10 @@ module.exports = {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
         },
+        'secondary-dark': {
+          DEFAULT: 'hsl(var(--secondary-dark))',
+          foreground: 'hsl(var(--secondary-dark-foreground))',
+        },
       },
       borderRadius: {
         lg: 'var(--radius)',
